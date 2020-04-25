ab121c16d7edaf949319abe107b21216e1f34a96
Tomek Stach <tomasz.stach@gmail.com>
2020-04-25 13:26:05 +0200

    Change invoices

--- templates/shaper_megadeal_ii/html/com_virtuemart/invoice/mail_html_shopperaddresses.php
+++ templates/shaper_megadeal_ii/html/com_virtuemart/invoice/mail_html_shopperaddresses.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  *
  * Layout for the order email
@@ -21,51 +22,55 @@
 // Check to ensure this file is included in Joomla!
 defined('_JEXEC') or die('Restricted access');
 ?>
-<table class="html-email" cellspacing="0" cellpadding="5" border="0" width="100%" style="border-collapse: collapse; font-family: Arial, Helvetica, sans-serif; font-size: 12px; margin: 0 auto;">
-	<tr>
-	<th width="50%" bgcolor="#EEEEEE" style="border: 1px solid #CCCCCC;">
-	    <?php echo vmText::_('COM_VIRTUEMART_USER_FORM_BILLTO_LBL'); ?>
-	</th>
-	<th width="50%" bgcolor="#EEEEEE" style="border: 1px solid #CCCCCC;">
-	    <?php echo vmText::_('COM_VIRTUEMART_USER_FORM_SHIPTO_LBL'); ?>
-	</th>
-    </tr>
-    <tr>
-	<td valign="top" width="50%" style="border: 1px solid #CCCCCC;">
+<table class="html-email" cellspacing="0" cellpadding="5" border="0" width="100%"
+  style="border-collapse: collapse; font-family: Arial, Helvetica, sans-serif; font-size: 12px; margin: 0 auto;">
+  <tr>
+    <th width="50%" bgcolor="#EEEEEE" style="border: 1px solid #CCCCCC;">
+      <?php echo vmText::_('COM_VIRTUEMART_USER_FORM_BILLTO_LBL'); ?>
+    </th>
+    <th width="50%" bgcolor="#EEEEEE" style="border: 1px solid #CCCCCC;">
+      <?php echo vmText::_('COM_VIRTUEMART_USER_FORM_SHIPTO_LBL'); ?>
+    </th>
+  </tr>
+  <tr>
+    <td valign="top" width="50%" style="border: 1px solid #CCCCCC;">
 
-	    <?php
+      <?php
 
-	    foreach ($this->userfields['fields'] as $field) {
-		if (!empty($field['value']) and $field['name'] != 'address_type_name') {
-			?><!-- span class="titles"><?php echo $field['title'] ?></span -->
-	    	    <span class="values vm2<?php echo '-' . $field['name'] ?>" ><?php echo $field['value'] ?></span>
-			<?php if ($field['name'] != 'title' and $field['name'] != 'first_name' and $field['name'] != 'middle_name' and $field['name'] != 'zip') { ?>
-			    <br class="clear" />
-			    <?php
-			}
-		    }
-		 
-	    }
-	    ?>
+      foreach ($this->userfields['fields'] as $field) {
+        if (!empty($field['value']) and $field['name'] != 'address_type_name') {
+          ?>
+      <!-- span class="titles"><?php echo $field['title'] ?></span -->
+      <span class="values vm2<?php echo '-' . $field['name'] ?>"><?php echo $field['value'] ?></span>
+      <?php if ($field['name'] != 'title' and $field['name'] != 'first_name' and $field['name'] != 'middle_name' and $field['name'] != 'zip') { ?>
+      <br class="clear" />
+      <?php
+          }
+        }
+      }
+      ?>
 
-	</td>
-	<td valign="top" width="50%" style="border: 1px solid #CCCCCC;">
-	    <?php
-	    foreach ($this->shipmentfields['fields'] as $field) {
+    </td>
+    <td valign="top" width="50%" style="border: 1px solid #CCCCCC;">
+      <?php
+      foreach ($this->shipmentfields['fields'] as $field) {
 
-		if (!empty($field['value']) and $field['name'] != 'address_type_name') {
-			    ?><!-- span class="titles"><?php echo $field['title'] ?></span -->
-			    <span class="values vm2<?php echo '-' . $field['name'] ?>" ><?php echo $field['value'] ?></span>
-			    <?php if ($field['name'] != 'title' and $field['name'] != 'first_name' and $field['name'] != 'middle_name' and $field['name'] != 'zip') { ?>
-		    	    <br class="clear" />
-				<?php
-			    }
-			}
-	    }
+        if (!empty($field['value']) and $field['name'] != 'address_type_name') {
+          ?>
+      <!-- span class="titles"><?php echo $field['title'] ?></span -->
+      <span class="values vm2<?php echo '-' . $field['name'] ?>"><?php echo $field['value'] ?></span>
+      <?php if ($field['name'] != 'title' and $field['name'] != 'first_name' and $field['name'] != 'middle_name' and $field['name'] != 'zip') { ?>
+      <br class="clear" />
+      <?php
+          }
+        }
+      }
 
-	    ?>
-	</td>
-    </tr>
-		<tr><td valign="top" width="50%" style="padding:5px"></td><td valign="top" width="50%" style="padding:5px"></td></tr>
+      ?>
+    </td>
+  </tr>
+  <tr>
+    <td valign="top" width="50%" style="padding:5px"></td>
+    <td valign="top" width="50%" style="padding:5px"></td>
+  </tr>
 </table>
-
